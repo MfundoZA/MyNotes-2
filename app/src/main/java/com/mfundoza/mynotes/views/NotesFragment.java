e3bbd97e26a0a5e553a9267f365bd4ded4cb8591
Mfundo <jordantenza@gmai.com>
2022-03-06 13:09:55 +0200

    Linked Fragment to it's ViewModel

--- app/src/main/java/com/mfundoza/mynotes/views/NotesFragment.java
+++ app/src/main/java/com/mfundoza/mynotes/views/NotesFragment.java
@@ -7,10 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.mfundoza.mynotes.R;
 import com.mfundoza.mynotes.databinding.FragmentNotesBinding;
+import com.mfundoza.mynotes.models.Note;
+import com.mfundoza.mynotes.viewmodels.NotesViewModel;
+
+import java.util.List;
 
 public class NotesFragment extends Fragment {
 
@@ -29,6 +35,11 @@ public class NotesFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        NotesViewModel notesViewModel = new ViewModelProvider(requireActivity()).get(NotesViewModel.class);
+        notesViewModel.getSavedNotes().observe(getViewLifecycleOwner(), new Observer<List<Note>>() {
+            // Todo create adapter class and assign the notes in notes in adapter here
+        });
 
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
